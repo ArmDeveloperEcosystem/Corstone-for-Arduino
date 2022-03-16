372a77a29338725c431ff13452fb3761d758681e
Sandeep Mistry <sandeep.mistry@arm.com>
2022-03-16 15:38:22 -0400

    Add defines for switches and LEDs

--- variants/corstone-300/variant.h
+++ variants/corstone-300/variant.h
@@ -11,4 +11,25 @@
 //
 //   https://gitlab.arm.com/iot/open-iot-sdk/mcu-driver-hal/mcu-driver-reference-platforms-for-arm/-/blob/release-0.2.2/mdh_arm/hal/inc/PinNames.h
 //
-#define LED_BUILTIN (200u)
+
+#define SW1 (100u)
+#define SW2 (101u)
+#define SW3 (102u)
+#define SW4 (103u)
+#define SW5 (104u)
+#define SW6 (105u)
+#define SW7 (106u)
+#define SW8 (107u)
+
+#define LED1 (200u)
+#define LED2 (201u)
+#define LED3 (202u)
+#define LED4 (203u)
+#define LED5 (204u)
+#define LED6 (205u)
+#define LED7 (206u)
+#define LED8 (207u)
+#define LED9 (208u)
+#define LED10 (209u)
+
+#define LED_BUILTIN LED1
